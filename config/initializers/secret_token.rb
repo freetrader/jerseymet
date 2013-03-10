1b52b357f5faeb7ca0973e457c49b0d840601a77
freetrader <phil.balderson@mac.com>
2013-03-10 13:21:38 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jerseymet::Application.config.secret_token = '7e9cfecb24b6ec1fc13ea1eef742f9ceab70438833a8c71fa59beba3e2df8303d1bb4183f56a162f3d2603a091a2644409ab81eac9eeb8062128b32cffc8068b'
